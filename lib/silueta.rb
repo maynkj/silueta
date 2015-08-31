12f3c23d3c591ff60cbbb13b6c6e21da1fd5ff3e
maynkj <mayn.kjaer@gmail.com>
2015-08-31 13:29:16 +0200

    Add attributes method.

--- lib/silueta.rb
+++ lib/silueta.rb
@@ -1,9 +1,29 @@
 require_relative "silueta/version"
 
 module Silueta
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def attribute(attr)
+      attributes << attr
+
+      attr_accessor(attr)
+    end
+
+    def attributes
+      return @attributes ||= []
+    end
+  end
+
   def initialize(attrs = {})
     attrs.each do |name, value|
       self.public_send("#{ name }=", value)
     end
+  end
+
+  def attributes
+    return self.class.attributes
   end
 end
--- test/silueta_test.rb
+++ test/silueta_test.rb
@@ -5,14 +5,20 @@ require_relative "../lib/silueta"
 class User
   include Silueta
 
-  attr_accessor :name, :email
+  attribute :name
+  attribute :email
 end
 
-scope do
-  test "initialize" do
-    user = User.new(name: "Mayn", email: "mayn@mail.com")
+test "initialize" do
+  user = User.new(name: "Mayn", email: "mayn@mail.com")
 
-    assert_equal("Mayn", user.name)
-    assert_equal("mayn@mail.com", user.email)
-  end
+  assert_equal("Mayn", user.name)
+  assert_equal("mayn@mail.com", user.email)
+end
+
+test "attributes" do
+  attributes = %i(name email)
+
+  assert_equal(attributes, User.attributes)
+  assert_equal(attributes, User.new.attributes)
 end
